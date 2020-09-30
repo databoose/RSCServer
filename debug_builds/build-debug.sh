ff255563c61254cac4e720a3ea5f12097815f61e
data <K1NGKAOS100@Gmail.com>
2020-09-30 06:56:37 -0700

    Made script more dynamic and added timer.c into debug build script

--- debug_builds/build-debug.sh
+++ debug_builds/build-debug.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
 rm debug-exec
-cd /home/data/Desktop/c_projects/LinkUpServer
-gcc -g -O0 -std=c99 main.c colors.c logger.c mysql.c cmd_input.c utils.c `mysql_config --cflags --libs` -lpthread -o debug_builds/debug-exec
+cd .. # nativgate up a directory
+printf "Compiling ...\n"
+gcc -g -O0 -std=c99 main.c colors.c logger.c mysql.c cmd_input.c utils.c timer.c `mysql_config --cflags --libs` -lpthread -o debug_builds/debug-exec
+
+gdb ./debug_builds/debug-exec # run the executable after done compiling
