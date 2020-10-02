89e30d063780ee01bcb0e8e029e5f93957be53b3
data <K1NGKAOS100@Gmail.com>
2020-10-02 00:56:30 -0700

    Cleaning up

--- mysql.c
+++ mysql.c
@@ -19,7 +19,7 @@ unsigned int flag = 0;
 
 char query[85]; // below 80 seems to cause buffer overflow from sprintf, beware
 
-int insert_query(MYSQL *conn, char* table, char* column1, char* column2, char* value1, char* value2)
+void insert_query(MYSQL *conn, char* table, char* column1, char* column2, char* value1, char* value2)
 {
     thread_logger *thl_insertquery = new_thread_logger(debug_mode);
     memset(query, '\0', sizeof(query));
@@ -27,19 +27,18 @@ int insert_query(MYSQL *conn, char* table, char* column1, char* column2, char* v
     // printf("Submitting query : %s\n", query);
 
     if(mysql_query(conn, query)) {
-        LOGF_DEBUG(thl_insertquery, 0 ,"MySQL query error : %s\n",mysql_error(conn), "printf"); // Returns the error message for the most recently invoked MySQL function. 
+        LOGF_ERROR(thl_insertquery, 0 ,"MySQL query error : %s\n",mysql_error(conn), "printf"); // Returns the error message for the most recently invoked MySQL function. 
     }
-       
+    
     else {
         printf("Successfully inserted\n\n");
     }
     clear_thread_logger(thl_insertquery);
 }
 
-int print_table_contents(MYSQL *conn, char* table)
+void print_table_contents(MYSQL *conn, char* table)
 {
     memset(query, '\0', sizeof(query)); // zeroing out query string incase some remains
-
     sprintf(query, "SELECT * FROM %s", table); // select all fields in table that we specify in the print_table_contents call
     if(mysql_query(conn, query)) {
         mysql_error(conn);
@@ -82,9 +81,11 @@ int mysql_main()
     {
         insert_query(conn, "user", "hwidhash_uid", "ip_address", "hashhere1", "iphere1");
         print_table_contents(conn,"user");
-        
-        clear_thread_logger(thl_mysqlmain);
+
         mysql_close(conn);
+        clear_thread_logger(thl_mysqlmain);
+
+        return 1;
     }
     
     else if(conn->thread_id == 0)
@@ -92,6 +93,9 @@ int mysql_main()
         LOGF_ERROR(thl_mysqlmain, 0 ,"Refusing to run mysql routine, mysql server most likely not running or something else horribly wrong.", "printf");
         mysql_close(conn);
         clear_thread_logger(thl_mysqlmain);
+
         return 0;
     }
+
+    return 1;
 }
