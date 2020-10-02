cf681e1b6ed09c4d41479c4d5ac5b7e927742f03
data <K1NGKAOS100@Gmail.com>
2020-10-02 00:51:43 -0700

    calls refactored to new, more specific/logical variable names

--- timer.c
+++ timer.c
@@ -28,10 +28,10 @@ void handle_timer(void *VPTR_THREAD_IP)
         }
     }
 
-    append_ipaddrtimer(THREAD_IP);
+    timer_append_ipaddr(THREAD_IP);
     
-    int TID = rand() % (999999999 + 1 - 100000000) + 100000000; // (max_number + 1 - minimum_number) + minimum_number
-    LOGF_DEBUG(thl_timer, 0, "Timer TID : %d", TID, NULL);
+    int TIMER_TID = rand() % (999999999 + 1 - 100000000) + 100000000; // (max_number + 1 - minimum_number) + minimum_number
+    LOGF_DEBUG(thl_timer, 0, "TIMER TID : %d", TIMER_TID, NULL);
 
     struct timerblock block = {.seconds_passed = 0, .times_ran = 0};
     struct timerblock longer = {.seconds_passed = 0, .times_ran = 0};
@@ -63,7 +63,7 @@ void handle_timer(void *VPTR_THREAD_IP)
             if (strcmp(ipsignal.SIGNAL_IP[i], THREAD_IP) == 0)
             { 
                 block.times_ran++;
-                LOGF_DEBUG(thl_timer, 0, "Timer thread (%d) : incrementing block.times_ran : %d", TID, block.times_ran, "printf");
+                LOGF_DEBUG(thl_timer, 0, "Timer thread (%d) : incrementing block.times_ran : %d", TIMER_TID, block.times_ran, "printf");
                 break;
             }
             
@@ -95,9 +95,9 @@ void handle_timer(void *VPTR_THREAD_IP)
                 if (block.times_ran >= 5)
                 {
                     LOGF_INFO(thl_timer, 0, "Timing out %s for 30 seconds", THREAD_IP, "printf");
-                    append_ipaddrban(THREAD_IP);
+                    banlist_append_ipaddr(THREAD_IP);
                     sleep(30);
-                    remove_ipaddrban(THREAD_IP);
+                    banlist_remove_ipaddr(THREAD_IP);
                 }
 
                 // always reset block values if seconds are equal to or above 8 no matter what
@@ -108,9 +108,9 @@ void handle_timer(void *VPTR_THREAD_IP)
     }
     while (detected == true);
 
-    printf("Exiting %s timer thread (TID : %d), no longer active\n", THREAD_IP, TID);
+    printf("Exiting %s timer thread (TIMER TID : %d), no longer active\n", THREAD_IP, TIMER_TID);
     clear_thread_logger(thl_timer);
-    remove_ipaddrtimer(THREAD_IP);
+    timer_remove_ipaddr(THREAD_IP);
     timer_thread_count--;
     pthread_exit(0);
 }
