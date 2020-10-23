d4804b601a3e92300e49bf88e5428734a74e7289
data <K1NGKAOS100@Gmail.com>
2020-10-22 22:43:36 -0700

    Added refactored 2D array signal_addresses to main.h for better consistency, also made sizes for the 2D arrays hardcoded in header as previous method of calculating them didn't work

--- include/main.h
+++ include/main.h
@@ -4,6 +4,10 @@
 #define TIMEOUT_IN 25 // seconds
 #define TIMEOUT_OUT 25
 
+#define BANNED_RAWLEN_SIZE 18
+#define TIMER_RAWLEN_SIZE 35
+#define IPSIGNAL_RAWLEN_SIZE 5
+
 enum THREAD_STORE_OPTION {
     create,
     update
@@ -26,8 +30,11 @@ int servsockfd; // global so we can pass it around functions
 
 pid_t self_pid;
 char appendcmd[64];
+
+// WARNING: ALSO CHANGE BANNED/TIMER/IPSIGNAL_RAWLEN_SIZE MACROS IF YOU CHANGE THE SIZE OF THESE
+char signal_addresses[5][16]; // stores up to 5 IP addresses at one time
 char banned_addresses[18][16];
-char timed_addresses[50][16];
+char timed_addresses[35][16];
 
 // these externs are defined and changed as needed in main.c (primarily)
 extern bool banning;
