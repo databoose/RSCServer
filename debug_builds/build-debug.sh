724a3b01fd12611c5aa69f9c97ea09d8bcbcd85b
data <K1NGKAOS100@Gmail.com>
2020-12-01 08:38:24 -0800

    Added additonal message when debug build is compiled

--- debug_builds/build-debug.sh
+++ debug_builds/build-debug.sh
@@ -5,4 +5,4 @@ cd .. # nativgate up a directory
 printf "Compiling ...\n"
 gcc -g -O0 -std=c99 main.c colors.c logger.c mysql.c cmd_input.c utils.c timer.c `mysql_config --cflags --libs` -lpthread -o debug_builds/debug-exec
 
-gdb ./debug_builds/debug-exec # run the executable after done compiling
+printf "Compiled\n"
