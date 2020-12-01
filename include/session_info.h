c3263418e89287287f6e79629bd499bd211cf435
data <K1NGKAOS100@Gmail.com>
2020-12-01 08:40:15 -0800

    Added in null macros for other int/enum variables, changed function parameters, added state enum, changed SessionInfo struct variable data types

--- include/session_info.h
+++ include/session_info.h
@@ -1,5 +1,31 @@
 #ifndef SESSIONINFO_H
 #define SESSIONINFO_H
-    char *NULLSTRING;
+    typedef enum {
+        READY,
+        BUSY
+    } state;
+
+    struct SessionInfoNode {
+        char *THREAD_IP;
+        char *HWID;
+        int CONNECTION_TID;
+
+        char *CONNECT_CODE;
+        state STATUS;
+        int ID;
+
+        struct SessionInfoNode* NEXT;
+        struct SessionInfoNode* PREV;
+    };
+    typedef struct SessionInfoNode SessionInfoNode_T;
+
     #define MAX_SESSION_STRUCTS 60 // this should never be a higher value than MAX_GLOBAL_THREADS
+    #define NO_ID 24706 // this is basically a unique null for when we have to pass the ID but don't want to
+    #define NO_CONNECTION_TID 38671
+    #define NO_STATUS 86284
+
+    void add_node(SessionInfoNode_T** head_ref, char *THREAD_IP, char *HWID, int CONNECTION_TID, char *CONNECT_CODE, state STATUS);
+    void delete_node(SessionInfoNode_T** head_ref, int ID);
+    void print_list(SessionInfoNode_T* head);
+    SessionInfoNode_T* find_node(SessionInfoNode_T* head, char *CONNECT_CODE, char *THREAD_IP, int ID);
 #endif
