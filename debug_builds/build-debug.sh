3a5d53eb130ec6b82ae90d6fb2cda248429c42a1
data <K1NGKAOS100@Gmail.com>
2020-12-06 23:54:10 -0800

    added session_info.c to compile in debug build script

--- debug_builds/build-debug.sh
+++ debug_builds/build-debug.sh
@@ -3,6 +3,6 @@
 rm debug-exec
 cd .. # nativgate up a directory
 printf "Compiling ...\n"
-gcc -g -O0 -std=c99 main.c colors.c logger.c mysql.c cmd_input.c utils.c timer.c `mysql_config --cflags --libs` -lpthread -o debug_builds/debug-exec
+gcc -g -O0 -std=c99 main.c colors.c logger.c mysql.c cmd_input.c utils.c timer.c session_info.c `mysql_config --cflags --libs` -lpthread -o debug_builds/debug-exec
 
 printf "Compiled\n"
